f8d3bc98c326aa1001be830bcc37305a80c299ab
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2025-10-19 10:57:20 +0530

    Update 2.2.c

--- Let_Us_C_Book/chapter_2/2.2.c
+++ Let_Us_C_Book/chapter_2/2.2.c
@@ -1,3 +1,4 @@
+// code which has limit for 5 digit number
 #include <stdio.h>
 #include <math.h>
 int main()
@@ -6,6 +7,30 @@ int main()
     long int rev_num;
     printf("enter the number which you want to reverse(number should be <32767):");
     scanf("%d",&n);
+    e = n%10;
+    n = n/10;
+    d = n%10;
+    n = n/10;
+    c = n%10;
+    n = n/10;
+    b = n%10;
+    n = n/10;
+    a = n%10;
+    rev_num = e *pow(10,4) + d *pow(10,3) + c *pow(10,2) + b *pow(10,1) + a;
+    printf("The reverse number is %ld",rev_num);
+    return 0;
+}
+
+
+// Another code which has no limit for 5 digit number 
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    long int n,a,b,c,d,e;
+    long int rev_num;
+    printf("enter the number which you want to reverse:");
+    scanf("%ld",&n);
     e = n%10;
     n = n/10;
     d = n%10;
