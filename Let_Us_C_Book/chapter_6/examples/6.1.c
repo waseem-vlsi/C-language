0deb4c87ea130f7e6f0f5f3d721a534f4cfc871d
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2025-12-26 07:41:42 +0530

    Create 6.1.c

---
+++ Let_Us_C_Book/chapter_6/examples/6.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n = 1;
+
+    printf("\nPrime numbers between 1 and 300 are:\n\t");
+
+    for (n = 1; n <= 300; n++)
+    {
+        i = 2;
+
+        for (i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                break;
+        }
+
+        if (i == n)
+            printf("%d\t", n);
+    }
+
+    return 0;
+}
