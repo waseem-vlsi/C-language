337e8b011d56406047234e186d2678321af1a8ce
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-01-07 10:22:42 +0530

    Create i.c

---
+++ Let_Us_C_Book/chapter_6/Exercise/i.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int i;
+   printf("\t\t\t1\n");
+   printf("\t\t2\t\t3\n");
+   printf("\t4\t\t5\t\t6\n");
+   printf("7\t\t8\t\t9\t\t10");
+   return 0;
+}
