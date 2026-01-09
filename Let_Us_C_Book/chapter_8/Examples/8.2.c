14bb175f66bccb9017ca519383fbf738f856b9eb
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-01-09 12:48:21 +0530

    Update 8.2.c

--- Let_Us_C_Book/chapter_8/Examples/8.2.c
+++ Let_Us_C_Book/chapter_8/Examples/8.2.c
@@ -20,3 +20,29 @@ int func_pow(int a,int b)
     y = pow(a,b);
     return y;
 }
+
+
+// ANOTHER METHOD
+
+#include <stdio.h>
+#include <math.h>
+float func_pow(float,float);
+int main()
+{
+    float a,b;
+    float result;
+    printf("Enter the value of a: ");
+    scanf("%f", &a);
+    printf("Enter the value of b: ");
+    scanf("%f", &b);
+    result = func_pow(a,b);
+    printf("The value of result is : %f", result);
+    return 0;
+}
+
+float func_pow(float a,float b)
+{
+    float y;
+    y = pow(a,b);
+    return y;
+}
