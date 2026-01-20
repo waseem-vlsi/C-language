2975fd29eedd3c81c87a8c880cd894ec058e1210
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-01-20 22:59:44 +0530

    Create Code.c

---
+++ Sum of Digits of a Number/Code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n = 1234, sum = 0;
+
+    while(n > 0) {
+        sum += n % 10;
+        n = n / 10;
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
