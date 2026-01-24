6f23a2c9ef3cd88d5de0cd300a68e04b30e4a9a6
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-01-24 22:20:02 +0530

    Create 3.c

---
+++ Let_Us_C_Book/Chapter_7/Practice_Examples/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char op;
+    float a, b;
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    switch(op)
+    {
+        case '+': printf("Result = %.2f\n", a + b); break;
+        case '-': printf("Result = %.2f\n", a - b); break;
+        case '*': printf("Result = %.2f\n", a * b); break;
+        case '/':
+            if(b != 0)
+                printf("Result = %.2f\n", a / b);
+            else
+                printf("Division by zero not allowed\n");
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+
+    return 0;
+}
