236febe2b9eb72f2c54ea91006079efd53c0c55a
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-02-07 22:19:51 +0530

    Create 12.c

---
+++ Let_Us_C_Book/Chapter_7/Practice_Examples/12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    switch(n % 2)
+    {
+        case 0:
+            printf("Even");
+            break;
+
+        case 1:
+            printf("Odd");
+            break;
+    }
+
+    return 0;
+}
