95eed840d7b0d46dffd55ce404baf042c2407d4f
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-02-26 22:02:00 +0530

    Create 8.c

---
+++ Let_Us_C_Book/chapter_9/Practice/8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *p = &a;
+    int **q = &p;
+
+    printf("a = %d\n", a);
+    printf("*p = %d\n", *p);
+    printf("**q = %d\n", **q);
+
+    return 0;
+}
+  
