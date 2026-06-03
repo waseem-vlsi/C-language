68f1f4332fd2bde2e7c5303213ad3001a2cf5c4a
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-03 09:51:41 +0530

    Update a.c

--- Let_Us_C_Book/chapter_8/Exercise/a.c
+++ Let_Us_C_Book/chapter_8/Exercise/a.c
@@ -12,3 +12,38 @@ int main()
 
     return 0;
 }
+
+
+// another method
+
+#include <stdio.h>
+int leap_year(int);
+
+int main()
+{
+    int year;
+    printf("Enter a year for which u need to find either it is a leap year or not: ");
+    scanf("%d", &year);
+    
+    leap_year(year);
+    return 0;
+}
+
+int leap_year(int year)
+{
+    
+    if(year%400 == 0)
+    {
+        printf("%d is a leap year", year);
+    }
+    else if((year%4 == 0) && (year%100 !=0) )
+    {
+        printf("%d is a leap year", year);
+    }
+    else 
+    {
+        printf("%d is not a leap year", year);
+    }
+    return 0;
+ 
+}
