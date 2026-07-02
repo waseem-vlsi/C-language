149b0c57405d08b37e4af752ee06ae1e3caae65c
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-07-02 11:25:06 +0530

    Update code.c

--- Arrays/practice/sum_of_array_elements/code.c
+++ Arrays/practice/sum_of_array_elements/code.c
@@ -13,3 +13,37 @@ int main()
     printf("sum : %0d", sum);
     return 0;
 }
+
+
+
+// mathod 2
+
+#include <stdio.h>
+
+int getSum(int arr[], int n );
+
+int main()
+{
+    int sum;
+    int arr[500];
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i = 0; i<n ; i++)
+    {
+        arr[i] = i;
+        printf("arr: %0d\t", arr[i]);
+    }
+    sum = getSum(arr,n);
+    printf("Sum : %0d", sum);
+    return 0;
+    }
+    
+    int getSum(int arr[], int n)
+    {
+        int sum = 0;
+        for(int j = 0; j<n; j++)
+        {
+        sum += arr[j];
+        }
+        return sum;
+    }
